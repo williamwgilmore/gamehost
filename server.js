0488003a44ebba082696457daac712d1030d0c73
DESKTOP-SP83B60\willi <williamwgilmore@gmail.com>
2017-04-28 22:38:23 -0500

    added ./ to the front of the path. I should not have used boilerplate code.

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ if (isDeveloping) {
     res.end();
   });
 } else {
-  app.use(express.static(__dirname + 'src'));
+  app.use(express.static(__dirname + './src'));
   app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'src/index.tpl.html'));
+    res.sendFile(path.join(__dirname, './src/index.tpl.html'));
   });
 }
 
